e355e227979aff35628978ae93b9e6a990261814
Jingqiang ZHANG <jqzhang@thoughtworks.com>
2014-09-02 15:05:00 +0800

    change file mode of rails.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140707111715) do
+ActiveRecord::Schema.define(version: 20140902060546) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
